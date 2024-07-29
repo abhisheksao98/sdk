23fc07bb1ae1951a4eb9a46030ae5d8dd2d9b3a6
Abhishek Sao <abhishek.sao@fstac.tech>
2024-07-29 11:09:34 +0530

    changes

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -1,6 +1,9 @@
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id ("maven-publish")
+
 }
 
 android {
@@ -31,9 +34,34 @@ android {
         jvmTarget = "1.8"
     }
 }
+afterEvaluate { ->
+    publishing {
+        publications {
+            create<MavenPublication>("aar") {
+                groupId = "com.fstac"
+                artifactId = "deviceutil"
+                version = "0.0.1"
+                // Specify the AAR file to publish
+                artifact(layout.buildDirectory.file("outputs/aar/sdk-debug.aar"))
+            }
+        }
+        repositories {
+            maven {
+
+                name = "Package"
+                url = uri("https://maven.pkg.github.com/abhisheksao98/sdk")
+//                credentials {
+//                     credentials.username = "abhisheksao98"
+//                    password = (project.findProperty("REPO_PASSWORD") ?: "").toString()
+//                }
+            }
+
+
+        }
+    }
+}
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
